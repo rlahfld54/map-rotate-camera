c04f5f68ec9fbb676ef0ef00261a5a8d4478f8ca
rlahfld54 <rlahfld54@naver.com>
2022-11-08 09:40:19 +0900

    진행 1

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Push an existing repository from the command line
+git remote add origin https://github.com/rlahfld54/map-rotate-camera.git
+git branch -M master
+git push -u origin master
+
+
 # [Vite Soft UI Dashboard](http://demos.creative-tim.com/vite-soft-ui-dashboard/?ref=readme-vitesud) [![Tweet](https://img.shields.io/twitter/url/http/shields.io.svg?style=social&logo=twitter)](https://twitter.com/intent/tweet?url=https://www.creative-tim.com/product/vue-soft-ui-dashboard&text=Check%20Vite%20Soft%20UI%20Dashboard%20made%20by%20@CreativeTim%20#webdesign%20#dashboard%20#softdesign%20#vue%20https://www.creative-tim.com/product/vite-soft-ui-dashboard)
 
 ![version](https://img.shields.io/badge/version-1.0.0-blue.svg) [![GitHub issues open](https://img.shields.io/github/issues/creativetimofficial/vue-soft-ui-dashboard.svg)](https://github.com/creativetimofficial/vite-soft-ui-dashboard/issues?q=is%3Aopen+is%3Aissue) [![GitHub issues closed](https://img.shields.io/github/issues-closed-raw/creativetimofficial/vite-soft-ui-dashboard.svg)](https://github.com/creativetimofficial/vite-soft-ui-dashboard/issues?q=is%3Aissue+is%3Aclosed)
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,10 +3,17 @@
     <div class="row">
       <div class="col">
         <div id="mapLayer">
-          <div id="point" style="position: absolute; top: 100px; left: 100px;">
-            <img id="look-point" src="../assets/Polygon 1.png" width="60" alt="화각">
-          </div>
-          <img @click="test" class="mapimg" src="../assets/7403915272e_l.jpg" width="600" alt="한반도">
+          <!-- 완성된 코드 -->
+            <div id="point" v-for="(item,index) in List" :key="index" 
+            @click="modifyData(item)"
+            :style="{top: item.x + 'px', left: item.y + 'px'}">
+              <img id="look-point" src="../assets/Polygon 1.png" width="50" height="70" alt="화각"
+              :style="{ transform: 'translate(-50%,-50%)'+`rotate(${item.angle}deg)` }"
+                style="top: -35px; left: 5px;">
+            </div>
+
+          <!-- 배경 이미지 -->
+          <img @click="getCoordinate" class="mapimg" src="../assets/7403915272e_l.jpg" width="600" alt="한반도">
         </div>
       </div>
       <div class="col">
@@ -21,7 +28,21 @@
 
           <h5>3. css transform을 이용해서 클릭시 원하는 각도만큼 회전하기</h5>
 
+          <h5>4. input창에 각도 같은 것 배열에 담아서 실제로 쓰는 것처럼 테스트 가능하게 만들기 </h5>
 
+        </div>
+
+        <div class="card mt-3">
+          <label for="각도" style="font-size: 20px;">각도 입력</label>
+          <input type="text" v-model.number="angle" />
+          <button @click="getAngle">각도 입력</button>
+          <br/>
+          <br/>
+          <div>
+            <h4>1. 맨처음에는 데이터가 이미 뿌려져 있다.</h4>
+            <h4>2. 이미 있는 데이터의 값을 수정해준다. ==> 빨간 점을 클릭하면 수정하는 함수가 실행된다.</h4>
+            <h4>3. 새로운 데이터 값을 추가해준다. ==> input을 통해 제어한다.</h4>
+          </div>
         </div>
       </div>
     </div>
@@ -33,10 +54,6 @@ import ActiveUsersChart from "@/examples/Charts/ActiveUsersChart.vue";
 import GradientLineChart from "@/examples/Charts/GradientLineChart.vue";
 import OrdersCard from "./components/OrdersCard.vue";
 import ProjectsCard from "./components/ProjectsCard.vue";
-import US from "../assets/img/icons/flags/US.png";
-import DE from "../assets/img/icons/flags/DE.png";
-import GB from "../assets/img/icons/flags/GB.png";
-import BR from "../assets/img/icons/flags/BR.png";
 
 export default {
   name: "DashboardDefault",
@@ -49,80 +66,76 @@ export default {
   },
   data() {
     return {
-      stats: {
-        iconBackground: "bg-gradient-success",
-        money: {
-          title: "Today's Money",
-          value: "$53,000",
-          percentage: "+55%",
-          iconClass: "ni ni-money-coins",
+      angle: 0,
+      List: [
+        { x: 338, y: 278 ,angle: 0},
+        { x: 272, y: 396 ,angle: 90},
+        { x: 475, y: 454 ,angle: 180},
+        { x: 113, y: 82 ,angle: 90},
+        { x: 60, y: 434 ,angle: 180},
+        { x: 91, y: 536 ,angle: 70},
+        { x: 231, y: 346 ,angle: 180},
+        { x: 412, y: 184 ,angle: 200},
+        { x: 540, y: 103 ,angle: 0}
+      ],
+      infoList: [
+        {
+          x: 5,
+          y: 40,
+          newAngle: 90
         },
-        users: {
-          title: "Today's Users",
-          value: "2,300",
-          percentage: "+3%",
-          iconClass: "ni ni-world",
-        },
-        clients: {
-          title: "New Clients",
-          value: "+3,462",
-          percentage: "-2%",
-          iconClass: "ni ni-paper-diploma",
-          percentageColor: "text-danger",
-        },
-        sales: {
-          title: "Sales",
-          value: "$103,430",
-          percentage: "+5%",
-          iconClass: "ni ni-cart",
-        },
-      },
-      sales: {
-        us: {
-          country: "United States",
-          sales: 2500,
-          value: "$230,900",
-          bounce: "29.9%",
-          flag: US,
-        },
-        germany: {
-          country: "Germany",
-          sales: "3.900",
-          value: "$440,000",
-          bounce: "40.22%",
-          flag: DE,
-        },
-        britain: {
-          country: "Great Britain",
-          sales: "1.400",
-          value: "$190,700",
-          bounce: "23.44%",
-          flag: GB,
-        },
-        brasil: {
-          country: "Brasil",
-          sales: "562",
-          value: "$143,960",
-          bounce: "32.14%",
-          flag: BR,
-        },
-      },
+        {
+          x: 100,
+          y: 100,
+          newAngle: 180
+        }
+      ]
     };
   },
   mounted() {
-    // var layer = document.getElementById('mapLayer');
-    // console.log(layer);
+  },
+  watch: {
+    // angle() {
+    //   console.log(typeof this.angle)
+    //   console.log(this.angle)
+    // }
   },
   methods: {
-    test(e) {
-      // console.log(e);
+    modifyData(item){
+      console.log(item); // 해당하는 정보의 값이 객체 형태로 나온다. 수정하고 싶은 값을 바꾼다.
+      item.x= 0;
+      item.y= 10;
+    },
+    getAngle() {
+      // 앵글을 바꿔주는 곳
+      console.log("getAngle");
+      const lookPoint = document.getElementById('look-point');
+      lookPoint.style.transform = `translate( -50%, -50%) rotate( ${this.angle}deg)`;
+      lookPoint.style.top = this.infoList[0].x.toString() + 'px';
+      lookPoint.style.left = this.infoList[0].y.toString() + 'px';
+      console.log(this.angle);
+    },
+    getCoordinate(e) {
+      // getCoordinate ==> 새로운 값을 추가 해줄때 필요한 함수
+      // 좌표 얻기 getCoordinate
       console.log(e.offsetX, e.offsetY);
+      let earth = {
+        x: 0, // top
+        y: 0, // left
+      };
 
-      // point의 css를 직접 바꿔준다.
+      earth.x = e.offsetX;
+      earth.y = e.offsetY;
+
+      // this.List = []; // 비워준다. 두개 이상 값이 못 들어가도록..
+      // this.List.push(earth);
+      // console.log(this.List);
+
+      // point의 좌표 css를 직접 바꿔준다.
       const point = document.getElementById('point');
       point.style.top = e.offsetY.toString() + 'px';
       point.style.left = e.offsetX.toString() + 'px';
-      // console.log(point.style.top, point.style.left);
+      console.log(point.style.top, point.style.left);
     }
   }
 };
@@ -141,24 +154,27 @@ export default {
   /* margin: auto; */
 }
 
+/* 동그란 원 */
 #point {
+  position: absolute;
   width: 10px;
   height: 10px;
   background: red;
+  border-radius: 50%;
+  transform-origin: center;
+  transform: translate(-50%, -50%);
   z-index: 9999;
 }
 
+/* 화각 이미지 */
 #look-point {
   position: absolute;
-  top: -84px;
-  left: -24px;
+  /* top: 5px;
+  left: 5px; */
+  /* top: -35px;
+  left: 5px; */
+  transform-origin: 25px 70px;
+  /* transform: rotate(90deg); */
   z-index: 9999;
-  transition: all ease 1s;
-}
-
-#look-point:hover{
-  top: -42px;
-  left: 32px;
-  transform: rotate( 90deg );
 }
 </style>
